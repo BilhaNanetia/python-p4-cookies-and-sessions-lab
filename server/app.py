a995463a01fccbdd87740c9a265199f7e33c9583
Bilha <bilha.leposo@student.moringaschool.com>
2024-07-03 11:57:17 +0300

    completes lab

--- server/app.py
+++ server/app.py
@@ -18,17 +18,28 @@ db.init_app(app)
 @app.route('/clear')
 def clear_session():
     session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
+    return {'message': '200: Successfully cleared session data'}, 200
 
 @app.route('/articles')
 def index_articles():
+    articles = [article.to_dict() for article in Article.query.all()]
+    return make_response(jsonify(articles), 200)
 
-    pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views',  0)
+    session['page_views'] += 1
 
-    pass
-
+    if session['page_views'] <= 3:
+        article = Article.query.filter(Article.id == id).first()
+        if article:
+            return article.to_dict(), 200
+        else:
+            return make_response({'message': 'Article not found'}, 404)
+        
+    return make_response({'message': 'Maximum pageview limit reached'}, 401)
+        
+    
 if __name__ == '__main__':
     app.run(port=5555)
